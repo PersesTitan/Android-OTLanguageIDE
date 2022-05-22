9e3a41bff05b64ee025c64767897b9a544c61eb7
PersesTitan <torampersestitan@gmail.com>
2022-05-23 07:50:40 +0900

    scanner add

--- app/src/main/java/com/example/android_otlanguageide/activity/variable/FloatP.java
+++ app/src/main/java/com/example/android_otlanguageide/activity/variable/FloatP.java
@@ -10,14 +10,17 @@ import com.example.android_otlanguageide.activity.item.KeyValueItem;
 import com.example.android_otlanguageide.activity.item.VariableWork;
 import com.example.android_otlanguageide.setting.Setting;
 
+import java.util.regex.Pattern;
+
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class FloatP extends Setting implements Check, VariableWork {
-
     private static final String SPECIFIED = "ㅇㅅㅇ";
+    private final String patternText = "\\n\\s*ㅇㅅㅇ\\s|^\\sㅇㅅㅇ\\s";
+    private final Pattern pattern = Pattern.compile(patternText);
 
     @Override
     public boolean check(String line) {
-        return line.trim().startsWith(SPECIFIED);
+        return pattern.matcher(line).find();
     }
 
     @Override
