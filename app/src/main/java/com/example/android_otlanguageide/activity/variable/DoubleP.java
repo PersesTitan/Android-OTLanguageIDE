b939d89b9406a52c98a1897f0a904ce5fafd653c
PersesTitan <torampersestitan@gmail.com>
2022-05-23 07:48:19 +0900

    scanner add

--- app/src/main/java/com/example/android_otlanguageide/activity/variable/DoubleP.java
+++ app/src/main/java/com/example/android_otlanguageide/activity/variable/DoubleP.java
@@ -9,14 +9,17 @@ import com.example.android_otlanguageide.activity.item.KeyValueItem;
 import com.example.android_otlanguageide.activity.item.VariableWork;
 import com.example.android_otlanguageide.setting.Setting;
 
+import java.util.regex.Pattern;
+
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class DoubleP extends Setting implements Check, VariableWork {
-
     private static final String SPECIFIED = "ㅇㅆㅇ";
+    private final String patternText = "\\n\\s*ㅇㅆㅇ\\s|^\\s*ㅇㄱㅇ\\s";
+    private final Pattern pattern = Pattern.compile(patternText);
 
     @Override
     public boolean check(String line) {
-        return line.trim().startsWith(SPECIFIED);
+        return pattern.matcher(line).find();
     }
 
     @Override
